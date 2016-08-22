b7d42af7b610d0f84104e55bc5e810f5b235d81f
Khoa Nguyen <hi@khoanguyen.me>
2016-08-23 03:11:05 +0700

    Empty hard-source-webpack-plugin cache when start a new dev server instance

--- src/builder/server.js
+++ src/builder/server.js
@@ -5,6 +5,8 @@ import webpackDevMiddleware from "webpack-dev-middleware"
 import webpackHotMiddleware from "webpack-hot-middleware"
 import historyFallbackMiddleware from "connect-history-api-fallback"
 import WebpackErrorNotificationPlugin from "webpack-error-notification"
+import findCacheDir from "find-cache-dir"
+import { emptyDirSync } from "fs-extra"
 
 import opn from "opn"
 import debug from "debug"
@@ -17,6 +19,7 @@ import collection from "../phenomic-loader/cache.js"
 import pathToUri from "../_utils/path-to-uri"
 
 const log = debug("phenomic:builder:server")
+const cacheDir = findCacheDir({ name: "phenomic-hard-source-wp" })
 
 export default (config) => {
   const { webpackConfigBrowser: webpackConfig } = config
@@ -40,6 +43,9 @@ export default (config) => {
     const devEntries = [
       require.resolve("webpack-hot-middleware/client"),
     ]
+
+    // Empty hard-source-webpack-plugin
+    emptyDirSync(cacheDir)
 
     const devConfig = {
       ...webpackConfig,
